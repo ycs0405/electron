a682be1d2368584772c13a8267c781b8380e0f76
deepak1556 <hop2deep@gmail.com>
2019-02-21 22:34:39 +0530

    Change signature of GetCookieListCallback to include list of excluded cookies
    https://chromium-review.googlesource.com/c/chromium/src/+/1416151

--- atom/browser/api/atom_api_cookies.cc
+++ atom/browser/api/atom_api_cookies.cc
@@ -159,7 +159,8 @@ void RunCallbackInUI(base::OnceClosure callback) {
 // Remove cookies from |list| not matching |filter|, and pass it to |callback|.
 void FilterCookies(std::unique_ptr<base::DictionaryValue> filter,
                    util::Promise promise,
-                   const net::CookieList& list) {
+                   const net::CookieList& list,
+                   const net::CookieStatusList& excluded_list) {
   net::CookieList result;
   for (const auto& cookie : list) {
     if (MatchesCookie(filter.get(), cookie))
